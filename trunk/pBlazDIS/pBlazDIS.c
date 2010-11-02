3ca125db1eb5a823f8ed8bfa311e50223434a16e
mediatronix@gmail.com <mediatronix@gmail.com@8b27110e-3afa-11de-8c0b-2fbac4a05959>
2010-11-02 11:26:11 +0000

    Clean up

--- trunk/pBlazDIS/pBlazDIS.c
+++ trunk/pBlazDIS/pBlazDIS.c
@@ -8,13 +8,13 @@
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  pBlazMRG is distributed in the hope that it will be useful,
+ *  pBlazDIS is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with pBlazASM.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with pBlazDIS.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <stdio.h>
--- trunk/pBlazDIS/pbLibgen.c
+++ trunk/pBlazDIS/pbLibgen.c
@@ -1,20 +1,20 @@
 /*
- *  Copyright © 2003..2008 : Henk van Kampen <henk@mediatronix.com>
+ *  Copyright © 2003..2010 : Henk van Kampen <henk@mediatronix.com>
  *
- *	This file is part of pBlazMRG.
+ *	This file is part of pBlazDIS.
  *
  *  pBlazMRG is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  pBlazMRG is distributed in the hope that it will be useful,
+ *  pBlazDIS is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with pBlazASM.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with pBlazDIS.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <stdlib.h>
