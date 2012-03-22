b23fa9edc6cf56b42f5f25b7d4f29a4cf8ec6f20
mediatronix <mediatronix@gmail.com>
2012-03-22 09:22:52 +0000

    Cleanup

--- pBlazBIT/pbTypes.h
+++ pBlazBIT/pbTypes.h
@@ -1,20 +1,21 @@
+
 /*
  *  Copyright © 2003..2012 : Henk van Kampen <henk@mediatronix.com>
  *
- *	This file is part of pBlazBS6.
+ *  This file is part of pBlazBIT.
  *
- *  pBlazBS6 is free software: you can redistribute it and/or modify
+ *  pBlazBIT is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  pBlazBS6 is distributed in the hope that it will be useful,
+ *  pBlazBIT is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with pBlazBS6.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with pBlazBIT.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 typedef unsigned char bool ;
