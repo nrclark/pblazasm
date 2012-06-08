acc526037bd195af3ada9040b7a9fd2c64157699
mediatronix <mediatronix@gmail.com>
2012-06-08 07:49:46 +0000

    IO processing script

---
+++ pBlazSIM/IO.js
@@ -0,0 +1,8 @@
+function setData( port, value ) {
+    print( "setData-> port: " + port + ", value: " + value ) ;
+}
+
+function getData( port ) {
+    print( "getData-> port: " + port ) ;
+    return port ;
+}
