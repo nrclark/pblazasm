a5eb357a47620cd7910c5135980300d878a90639
mediatronix <mediatronix@gmail.com>
2013-02-10 15:47:27 +0000

    added separate settings handler qtpropertybrowser is subtree now

--- pBlazBLD/projecthandler.cpp
+++ pBlazBLD/projecthandler.cpp
@@ -18,56 +18,24 @@ QmtxProjectHandler::QmtxProjectHandler( QObject * parent ) {
 }
 
 void QmtxProjectHandler::Init() {
+    QtBrowserItem * britem ;
+
     modified = false ;
     variantManager->clear() ;
 
-    //variantManager->valueChanged();
+    // variantManager->valueChanged();
 
-    variantEditor->setFont( *fixedFont ) ;
+    variantEditor->setFont( * fixedFont ) ;
     variantEditor->setAlternatingRowColors( false ) ;
     variantEditor->setFactoryForManager( (QtVariantPropertyManager *)variantManager, variantFactory ) ;
     variantEditor->setPropertiesWithoutValueMarked(true);
     variantEditor->setRootIsDecorated( true ) ;
     variantEditor->setResizeMode( QtTreePropertyBrowser::ResizeToContents ) ;
 
-    QtBrowserItem * britem ;
-
     projectItem = variantManager->addProperty(QVariant::String, QLatin1String(" Project Name"));
     britem = variantEditor->addProperty( projectItem );
     variantEditor->setBackgroundColor( britem, QColor( 240, 240, 240, 255 ) );
-    projectItem->setValue( "<untitled>" );
-
-
-    toolsItem = variantManager->addProperty(VariantManager::groupTypeId(),  QLatin1String(" Tools"));
-    britem = variantEditor->addProperty( toolsItem ) ;
-    variantEditor->setBackgroundColor( britem, QColor( 250, 240, 255, 255 ) ) ;
-
-    toolsASMfile = variantManager->addProperty(VariantManager::filePathTypeId(), QLatin1String("pBlazASM file path"));
-    toolsASMfile->setValue( "./pBlazASM.exe" ) ;
-    toolsASMfile->setAttribute( "filter", "EXE files (*.exe);;All files (*.*)" ) ;
-    toolsItem->addSubProperty( toolsASMfile ) ;
-
-    toolsMEMfile = variantManager->addProperty(VariantManager::filePathTypeId(), QLatin1String("pBlazMEM file path"));
-    toolsMEMfile->setValue( "./pBlazMEM.exe" ) ;
-    toolsMEMfile->setAttribute( "filter", "EXE files (*.exe);;All files (*.*)" ) ;
-    toolsItem->addSubProperty( toolsMEMfile ) ;
-
-    toolsBITfile = variantManager->addProperty(VariantManager::filePathTypeId(), QLatin1String("pBlazBIT file path"));
-    toolsBITfile->setValue( "./pBlazBIT.exe" ) ;
-    toolsBITfile->setAttribute( "filter", "EXE files (*.exe);;All files (*.*)" ) ;
-    toolsItem->addSubProperty( toolsBITfile ) ;
-
-
-    editorItem = variantManager->addProperty(VariantManager::groupTypeId(),  QLatin1String(" Editor options"));
-    britem = variantEditor->addProperty( editorItem ) ;
-    variantEditor->setBackgroundColor( britem, QColor( 250, 240, 240, 255 ) ) ;
-
-    fontItem = variantManager->addProperty( QVariant::Font, QLatin1String(" Font Property") ) ;
-    variantManager->setValue( fontItem, *fixedFont ) ;
-    editorItem->addSubProperty( fontItem ) ;
-    fontItem = variantManager->addProperty(QVariant::SizePolicy, QLatin1String(" SizePolicy Property" ) ) ;
-    editorItem->addSubProperty( fontItem ) ;
-    variantEditor->setExpanded( britem, false ) ;
+    projectItem->setValue( "<untitled>" ) ;
 
     // pBlazASM
     asmItem = variantManager->addProperty(VariantManager::groupTypeId(), QLatin1String(" pBlazASM options"));
@@ -234,6 +202,24 @@ QStringList QmtxProjectHandler::asmArguments() {
     return args ;
 }
 
+QStringList QmtxProjectHandler::mrgArguments() {
+    QStringList args ;
+
+    if ( mrgOptVerbose->value().toBool() )
+        args << "-v" ;
+
+    return args ;
+}
+
+QStringList QmtxProjectHandler::bitArguments() {
+    QStringList args ;
+
+    if ( bitOptVerbose->value().toBool() )
+        args << "-v" ;
+
+    return args ;
+}
+
 QString QmtxProjectHandler::fileName() {
     return projectFileName ;
 }
@@ -295,6 +281,8 @@ bool QmtxProjectHandler::load_file() {
     int size ;
 
     Init() ;
+
+    projectItem->setValue( projectFileName ) ;
     bitOptVerbose->setValue( settings.value("pBlazBIT/options/verbose").toBool() ) ;
 
     settings.beginGroup( "pBlazASM" ) ;
---
+++ pBlazBLD/settingshandler.cpp
@@ -0,0 +1,125 @@
+#include <QDebug>
+
+#include "settingshandler.h"
+#include "qxmlsettings.h"
+
+QmtxSettingsHandler::QmtxSettingsHandler( QObject * parent ) {
+    Q_UNUSED( parent ) ;
+
+    variantManager = new VariantManager() ;
+    variantFactory = new VariantFactory() ;
+    variantEditor = new QtTreePropertyBrowser(  ) ;
+    fixedFont = new QFont( "Consolas [Monaco]", 9 ) ;
+
+//    QObject::connect( (QtVariantPropertyManager *)variantManager, SIGNAL( valueChanged(QtProperty *, const QVariant &) ),
+//                      (QObject *)this, SLOT( setModified(QtProperty *, const QVariant &) ) ) ;
+
+    Init();
+}
+
+void QmtxSettingsHandler::Init() {
+    QtBrowserItem * britem ;
+
+    variantManager->clear() ;
+
+    // variantManager->valueChanged();
+
+    variantEditor->setFont( * fixedFont ) ;
+    variantEditor->setAlternatingRowColors( false ) ;
+    variantEditor->setFactoryForManager( (QtVariantPropertyManager *)variantManager, variantFactory ) ;
+    variantEditor->setPropertiesWithoutValueMarked(true);
+    variantEditor->setRootIsDecorated( true ) ;
+    variantEditor->setResizeMode( QtTreePropertyBrowser::ResizeToContents ) ;
+
+    toolsItem = variantManager->addProperty(VariantManager::groupTypeId(),  QLatin1String(" Tools"));
+    britem = variantEditor->addProperty( toolsItem ) ;
+    variantEditor->setBackgroundColor( britem, QColor( 250, 240, 255, 255 ) ) ;
+
+    toolsASMfile = variantManager->addProperty(VariantManager::filePathTypeId(), QLatin1String("pBlazASM file path"));
+    toolsASMfile->setValue( "./pBlazASM.exe" ) ;
+    toolsASMfile->setAttribute( "filter", "EXE files (*.exe);;All files (*.*)" ) ;
+    toolsItem->addSubProperty( toolsASMfile ) ;
+
+    toolsMRGfile = variantManager->addProperty(VariantManager::filePathTypeId(), QLatin1String("pBlazMEM file path"));
+    toolsMRGfile->setValue( "./pBlazMEM.exe" ) ;
+    toolsMRGfile->setAttribute( "filter", "EXE files (*.exe);;All files (*.*)" ) ;
+    toolsItem->addSubProperty( toolsMRGfile ) ;
+
+    toolsBITfile = variantManager->addProperty(VariantManager::filePathTypeId(), QLatin1String("pBlazBIT file path"));
+    toolsBITfile->setValue( "./pBlazBIT.exe" ) ;
+    toolsBITfile->setAttribute( "filter", "EXE files (*.exe);;All files (*.*)" ) ;
+    toolsItem->addSubProperty( toolsBITfile ) ;
+
+
+    editorItem = variantManager->addProperty(VariantManager::groupTypeId(),  QLatin1String(" Editor options"));
+    britem = variantEditor->addProperty( editorItem ) ;
+    variantEditor->setBackgroundColor( britem, QColor( 250, 240, 240, 255 ) ) ;
+
+    fontItem = variantManager->addProperty( QVariant::Font, QLatin1String(" Font Property") ) ;
+    variantManager->setValue( fontItem, *fixedFont ) ;
+    editorItem->addSubProperty( fontItem ) ;
+    fontItem = variantManager->addProperty(QVariant::SizePolicy, QLatin1String(" SizePolicy Property" ) ) ;
+    editorItem->addSubProperty( fontItem ) ;
+    variantEditor->setExpanded( britem, false ) ;
+}
+
+void QmtxSettingsHandler::Read() {
+    QSettings settings( "Mediatronix", "pBlazBLD" ) ;
+    setPBlazASM( settings.value( "pBlazASM", "./pBlazASM" ).toString() ) ;
+    setPBlazMRG( settings.value( "pBlazMRG", "./pBlazMRG" ).toString() ) ;
+    setPBlazBIT( settings.value( "pBlazBIT", "./pBlazBIT" ).toString() ) ;
+}
+
+void QmtxSettingsHandler::Write() {
+    QSettings settings( "Mediatronix", "pBlazBLD" ) ;
+    settings.setValue( "pBlazASM", pBlazASM() ) ;
+    settings.setValue( "pBlazMRG", pBlazMRG() ) ;
+    settings.setValue( "pBlazBIT", pBlazBIT() ) ;
+}
+
+QString QmtxSettingsHandler::pBlazASM() {
+    if ( toolsASMfile )
+        return toolsASMfile->valueText() ;
+    return QString() ;
+}
+
+QString QmtxSettingsHandler::pBlazMRG() {
+    if ( toolsMRGfile )
+        return toolsMRGfile->valueText() ;
+    return QString() ;
+}
+
+QString QmtxSettingsHandler::pBlazBIT() {
+    if ( toolsBITfile )
+        return toolsBITfile->valueText() ;
+    return QString() ;
+}
+
+void QmtxSettingsHandler::setPBlazASM(QString path) {
+    if ( toolsASMfile )
+        toolsASMfile->setValue( path ) ;
+}
+
+void QmtxSettingsHandler::setPBlazMRG(QString path) {
+    if ( toolsMRGfile )
+        toolsMRGfile->setValue( path ) ;
+}
+
+void QmtxSettingsHandler::setPBlazBIT(QString path) {
+    if ( toolsBITfile )
+        toolsBITfile->setValue( path ) ;
+}
+
+QmtxSettingsHandler::~QmtxSettingsHandler() {
+    delete variantManager;
+    delete variantFactory;
+}
+
+QtTreePropertyBrowser * QmtxSettingsHandler::getVariantEditor() {
+    return variantEditor ;
+}
+
+QFont QmtxSettingsHandler::getFont() {
+    return *fixedFont ;
+}
+
